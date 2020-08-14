db85fdaa2f2aa103736650017bbff95ebc94742d
antman666 <945360554@qq.com>
2020-08-14 10:09:55 +0800

    dwm

--- config.h
+++ config.h
@@ -77,10 +77,10 @@ static const char *termcmd[] 		 = { "st", NULL };
 static const char *browsercmd[] 	 = { "google-chrome-stable", NULL };
 
 static Key keys[] = {
-	/* modifier            key                      function        argument */
+	/* modifier     	        key        function        argument */
 	{ MODKEY,			XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,			XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,			XK_n,	   spawn,	   {.v = bordercmd } },
+	{ MODKEY,			XK_n,	   spawn,	   {.v = browsercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },

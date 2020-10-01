2cefb9087872097e5f11bcc0d630cbfb4b2e16d8
antman666 <945360554@qq.com>
2020-10-01 11:12:48 +0800

    dwm

--- config.h
+++ config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2]			 = "0"; 	 /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]		 = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] 		 = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[] 		 = { "st", NULL };
 static const char *browsercmd[] 	 = { "google-chrome-stable", NULL };
 static const char *musiccmd[]		 = { "netease-cloud-music", NULL };
@@ -80,7 +80,7 @@ static const char *screenshotcmd[]	 = { "/home/antman/Desktop/scripts/screenshot
 
 static Key keys[] = {
 	/* modifier     	        key        function        argument */
-	{ MODKEY,			XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY, 			XK_d, 	   spawn, 	   {.v = dmenucmd } },
 	{ MODKEY,			XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_n,	   spawn,	   {.v = browsercmd } },
 	{ MODKEY|ShiftMask,		XK_m,	   spawn,	   {.v = musiccmd } },
@@ -93,7 +93,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z,      hidewin,        {0} },

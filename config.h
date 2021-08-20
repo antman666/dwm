27a6a9c83230598c043366f4eb3c85fc9a3a6ce2
antman666 <945360554@qq.com>
2021-08-20 23:23:32 +0800

    make it much prettier

--- config.h
+++ config.h
@@ -1,38 +1,37 @@
 /* appearance */
-static const unsigned int borderpx = 2; /* border pixel of windows */
-static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2; /* systray spacing */
-static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display
-systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray = 1;             /* 0 means no systray */
-static const int smartgaps = 0; /* 1 means no outer gap when there is only one window */
-static const int showbar = 1;   /* 0 means no bar */
-static const int topbar = 0;    /* 0 means bottom bar */
-static const int viewontag = 1; /* Switch view on tag switch */
-static const char *fonts[] = {"SauceCodePro Nerd Font Mono:size=15"};
-static const char dmenufont[] = "SauceCodePro Nerd Font Mono:size=15";
-static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#bbbbbb";
-static const char col_gray4[] = "#ffffff";
-static const char col_cyan[] = "#37474F";
-static const char col_border[] = "#42A5F5";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
-static const char music_player[] = "yesplaymusic";
-static const char default_browser[] = "google-chrome-stable";
-static const char default_screen_lock[] = "slock";
+static const unsigned int borderpx       = 2; 	/* border pixel of windows */
+static const unsigned int snap           = 32;  /* snap pixel */
+static const unsigned int systraypinning = 0; 	/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2; 	/* systray spacing */
+static const int systraypinningfailfirst = 1; 	/* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;   /* 0 means no systray */
+static const int smartgaps               = 0; 	/* 1 means no outer gap when there is only one window */
+static const int showbar                 = 1;   /* 0 means no bar */
+static const int topbar                  = 0;   /* 0 means bottom bar */
+static const int viewontag               = 1; 	/* Switch view on tag switch */
+static const char *fonts[]               = {"SauceCodePro Nerd Font Mono:size=15"};
+static const char dmenufont[]            = "SauceCodePro Nerd Font Mono:size=15";
+static const char col_gray1[]            = "#222222";
+static const char col_gray2[]            = "#444444";
+static const char col_gray3[]            = "#bbbbbb";
+static const char col_gray4[]            = "#ffffff";
+static const char col_cyan[]             = "#37474F";
+static const char col_border[]           = "#42A5F5";
+static const unsigned int baralpha       = 0xd0;
+static const unsigned int borderalpha    = OPAQUE;
+static const char music_player[]         = "yesplaymusic";
+static const char default_browser[]      = "google-chrome-stable";
+static const char default_screen_lock[]  = "slock";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-    [SchemeSel] = {col_gray4, col_cyan, col_border},
-    [SchemeHid] = {col_cyan, col_gray1, col_border},
+    [SchemeSel]  = {col_gray4, col_cyan, col_border},
+    [SchemeHid]  = {col_cyan, col_gray1, col_border},
 };
 static const unsigned int alphas[][3] = {
     /*               fg      bg        border     */
     [SchemeNorm] = {OPAQUE, baralpha, borderalpha},
-    [SchemeSel] = {OPAQUE, baralpha, borderalpha},
+    [SchemeSel]  = {OPAQUE, baralpha, borderalpha},
 };
 
 /* tagging */
@@ -43,17 +42,14 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING)  = title
      */
-    /* class      		instance    title       tags mask     isfloating
-     * monitor
-     */
-    {"Gimp", NULL, NULL, 0, 1, -1},
+    /* class 	instance    title       tags 	mask    isfloating 	monitor */
+    {"Gimp", 	NULL, 		NULL, 		0, 		1, 		-1},
 };
 
 /* layout(s) */
-static const float mfact = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster = 1;   /* number of clients in master area */
-static const int resizehints =
-    1; /* 1 means respect size hints in tiled resizals */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;   /* number of clients in master area */
+static const int resizehints = 1; 	/* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -64,11 +60,11 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY, TAG)                                                      \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
-      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
-      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
-      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+#define TAGKEYS(KEY, TAG)                                                      		\
+		{MODKEY, 							KEY, view, 			{.ui = 1 << TAG}},  \
+		{MODKEY | ControlMask, 				KEY, toggleview, 	{.ui = 1 << TAG}},  \
+		{MODKEY | ShiftMask, 				KEY, tag, 			{.ui = 1 << TAG}},  \
+		{MODKEY | ControlMask | ShiftMask, 	KEY, toggletag, 	{.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                             \
@@ -77,24 +73,19 @@ static const Layout layouts[] = {
   }
 
 /* commands */
-static char dmenumon[2] =
-    "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL
 };
-static const char *termcmd[] = {"st", NULL};
-static const char *browsercmd[] = {default_browser, NULL};
-static const char *musiccmd[] = {music_player, NULL};
-static const char *screenlock[] = {default_screen_lock, NULL};
-static const char *volup[] = {"/home/antman/Desktop/scripts/volup.sh", NULL};
-static const char *voldown[] = {"/home/antman/Desktop/scripts/voldown.sh",
-                                NULL
-                               };
-static const char *screenshotcmd[] = {"scrot", "-bq", "100", "screenshot.png"};
-static const char *screenshotchoosecmd[] = {"scrot", "-sq", "100",
-                                            "screenget.png"
-                                           };
+static const char *termcmd[]             = {"st", NULL};
+static const char *browsercmd[]          = {default_browser, NULL};
+static const char *musiccmd[]            = {music_player, NULL};
+static const char *screenlock[]          = {default_screen_lock, NULL};
+static const char *volup[]               = {"/home/antman/Desktop/scripts/volup.sh", NULL};
+static const char *voldown[]             = {"/home/antman/Desktop/scripts/voldown.sh",NULL};
+static const char *screenshotcmd[]       = {"scrot", "-bq", "100", "screenshot.png"};
+static const char *screenshotchoosecmd[] = {"scrot", "-sq", "100","screenget.png"};
 
 static Key keys[] = {
     /* modifier              key        	function        argument */
